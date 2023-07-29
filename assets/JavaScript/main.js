d012e0a11d150a04df4116eae8b30549affc14ca
JeerTeles <jadersoncontatos@gmail.com>
2023-07-29 00:49:00 -0300

    Js

---
+++ assets/JavaScript/main.js
@@ -0,0 +1,32 @@
+const modalContainer = document.getElementById("modalContainer");
+const openModal = document.getElementById("openModal");
+const youtubeIframe = document.getElementById("youtubeIframe");
+
+openModal.addEventListener("click", function(){
+    modalContainer.style.display = 'block';
+})
+
+window.addEventListener("click", function(e){
+    if(e.target == modalContainer){
+        modalContainer.style.display = 'none'
+        youtubeIframe.src = youtubeIframe.src;
+    }
+})
+
+function playAudio() {
+    var audio = document.getElementById("music");
+    var speakerSlash = document.getElementsByClassName("ph-speaker-slash")[0];
+    var speakerHigh = document.getElementsByClassName("ph-speaker-high")[0];
+  
+    if (!audio.paused && !audio.ended) {
+      audio.pause();
+      speakerSlash.style.display = "block";
+      speakerHigh.style.display = "none";
+    } else {
+      audio.play();
+      speakerSlash.style.display = "none";
+      speakerHigh.style.display = "block";
+    }
+  }
+
+
